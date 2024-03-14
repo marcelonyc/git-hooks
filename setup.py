e36bd5d66d7d551f593213f52950f133a456d55d
marcelonyc <marcelo.litovsky@gmail.com>
2024-03-14 11:40:23 -0400

    Added version

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from pathlib import Path
-
 from setuptools import find_packages
 from setuptools import setup
+
+
+def get_version():
+    """Parse package __version__.py to get version."""
+    versionpy = (Path("python_projects/src") / "__version__.py").read_text()
+    return versionpy.split("'")[1]
+
+
+VERSION = get_version()
 
 
 setup(
     name="JFrog Audit",
     packages=find_packages(exclude=(["test*", "tmp*"])),
     description="Audit code before commit",
+    version=VERSION,
     author="JFrog",
     author_email="marcelol@jfrog.com",
     url="https://github.com/marcelonyc/jfrog-pre-commit",
